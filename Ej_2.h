9e6ac4cb168c3dad47ec7fa3a327eab15b3de5aa
balta2205 <baltasar.oostdijk@gmail.com>
2025-04-01 15:15:13 -0300

    Ej_2: Avance de las clases (hasta inciso b -> iii)

--- Ej_2.h
+++ Ej_2.h
@@ -7,35 +7,6 @@
 #include <string>
 #include <utility> // Para std::pair 
 
-/*
-2. Escribir una clase denominada Curso que contiene un vector (std::vector) con
-punteros a objetos estudiantes.
-
-a. El objeto estudiante simplemente cuenta con el nombre completo, su legajo
-(que es único por alumno), una lista de cursos con su nota final y los métodos
-que crea necesarios para obtener los datos del alumno: nombre completo,
-legajo y su promedio general. En función de esto, califique todos los atributos
-correctamente.
-b. Como se mencionó anteriormente, un objeto de la clase Curso contendrá la lista
-de estudiantes del curso (el vector conteniendo objetos tipo estudiante). La
-clase Curso permite:
-i. Inscribir y desinscribir estudiantes al curso.
-ii. Ver si un estudiante se encuentra inscripto o no en el curso buscándolo por
-su legajo.
-iii. Indicar si el curso está completo o no, teniendo en cuenta que el curso tiene
-una capacidad de 20 alumnos.
-iv. Imprimir la lista de estudiantes en orden alfabético. Para ello, utilice el
-algoritmo std::sort() en <algorithm>, el cual requerirá sobreescribir el
-operador “<”, y sobreescriba el operador “<<” (del método y clase que
-correspondan) para presentar los datos por pantalla.
-v. Dado que algunos cursos comparten la mayor parte de los estudiantes, se
-desea poder hacer una copia del objeto curso. Justifique su respuesta con
-un comentario en el código (esta puede llevar varias líneas), indicando de
-que tipo de copia se trata y como la hizo.
-c. ¿Qué tipo de relación existe entre los objetos curso y estudiante?
-d. Proporcione un menú que permita evaluar lo pedido en este ejercicio
-*/
-
 class Estudiante {
 private:
     const std::string nombre;
@@ -44,7 +15,7 @@ private:
 
 public:
     // Constructor
-    Estudiante(std::string nombre, int leg) : nombre(std::move(nombre)), legajo(leg) {}
+    Estudiante(std::string nombre, int leg);
 
     //Destructor
     ~Estudiante() = default;
@@ -82,7 +53,7 @@ public:
     void inscribirEstudiante(std::unique_ptr<Estudiante> estudiante);
     void desinscribirEstudiante(int legajo);
     bool buscarEstudiante(int legajo);
-    bool capacidad();
+    bool completo();
     void imprimirEstudiantes();
     void ordenarEstudiantes();
 };
--- Ej_2_source.cpp
+++ Ej_2_source.cpp
@@ -1,23 +1,24 @@
 #include "Ej_2.h"
 
 /*
-ii. Ver si un estudiante se encuentra inscripto o no en el curso buscándolo por
-su legajo.
-iii. Indicar si el curso está completo o no, teniendo en cuenta que el curso tiene
-una capacidad de 20 alumnos.
+
 iv. Imprimir la lista de estudiantes en orden alfabético. Para ello, utilice el
 algoritmo std::sort() en <algorithm>, el cual requerirá sobreescribir el
 operador “<”, y sobreescriba el operador “<<” (del método y clase que
 correspondan) para presentar los datos por pantalla.
+
 v. Dado que algunos cursos comparten la mayor parte de los estudiantes, se
 desea poder hacer una copia del objeto curso. Justifique su respuesta con
 un comentario en el código (esta puede llevar varias líneas), indicando de
 que tipo de copia se trata y como la hizo.
+
 c. ¿Qué tipo de relación existe entre los objetos curso y estudiante?
 d.*/
 
+// Clase Estudiante
+
 // Constructor.
-Estudiante::Estudiante(std::string nombre, int legajo) : nombre(nombre), legajo(legajo) {}
+Estudiante::Estudiante(std::string name, int leg) : nombre(std::move(name)), legajo(leg) {}
 
 // Metodos getters.
 std::string Estudiante::getNombre() const {return nombre;} // Devuelve el nombre.
@@ -27,49 +28,84 @@ int Estudiante::getLegajo() const {return legajo;} // Devuelve el legajo
 double Estudiante::getPromedio() const { // Devuelve el promedio de las notas 
     if(cursosNotas.empty()) return 0.0;
     double promedio = 0.0;
-    for(int i = 0; i < cursosNotas.size(); i++){
-        promedio += cursosNotas[i].second;
-    }
+    for(int i = 0; i < cursosNotas.size(); i++) promedio += cursosNotas[i].second;
 
     return promedio / cursosNotas.size();
 }
 
 // Metodo para agregar un curso y su nota.
-void Estudiante::agregarCursoNota(std::string curso, double nota) {cursosNotas.push_back(std::make_pair(curso, nota));}
-
-void Estudiante::eliminarCursoNota(std::string curso){
-
-    for(int i = 0; i < cursosNotas.size(); i++){
-        if(cursosNotas[i].first == curso){
-            cursosNotas.erase(cursosNotas.begin() + i);
-            return;
+void Estudiante::agregarCursoNota(std::string curso, double nota) {
+    for(int i = 0; i < cursosNotas.size(); i++) {
+        if (cursosNotas[i].first == curso) {
+            std::cout << "El curso ya esta anotado con nombre: " << cursosNotas[i].first << ", y nota: " << cursosNotas[i].second << "." << std::endl;
+            return; 
         }
+    }
+    cursosNotas.push_back(std::make_pair(curso, nota));
+    return;
+}
+
+// Metodo para eliminar un curso y su nota.
+void Estudiante::eliminarCursoNota(std::string curso){ 
+    for (int i = 0; i < cursosNotas.size(); i++) {
+        if(cursosNotas[i].first == curso) {
+            cursosNotas.erase(cursosNotas.begin() + i); 
+            return;
+        } 
     }
 }
 
-Curso::Curso(std::string nombre) : {nombre(nombre);} 
 
+// Clase Curso.
+
+// Constructor.
+Curso::Curso(std::string nombre) : nombre(std::move(nombre)) {}
+
+// Metodos Getters.
 std::string Curso::getNombre(){return nombre;}
 
+// Metodo para inscribir un estudiante. -> Inciso b) -> i)
 void Curso::inscribirEstudiante(std::unique_ptr<Estudiante> estudiante){
-    if (capacidad_max == 20){
-        std::cout << "Cupos llenos." << std::endl;
-        return;
+    if (completo()){ std::cout << "Cupos llenos." << std::endl; return;}
+
+    if(buscarEstudiante(estudiante -> getLegajo())){ 
+        std::cout << "El estudiante ya esta inscripto en este curso." <<std::endl; 
+        return; 
     }
+
     lista_estudiantes.push_back(estudiante);
     return;
 }
 
+// Metodo para desinscribir un estudiante. -> Inciso b) -> i)
 void Curso::desinscribirEstudiante(int legajo){
 
-    for(int i = 0; i < lista_estudiantes.size(); i++){
-        if(lista_estudiantes[i].getLegajo() == legajo){
-            lista_estudiantes[i].eliminarCursoNota(this.getNombre())
-            lista_estudiantes.erase(i);
-            return;
-        }
+    if (!buscarEstudiante(legajo)) {
+        std::cout << "El estudiante con el legajo " << legajo << " no estaba inscripto." << std::endl;
+        return;
     }
 
-    std::cout << "El estudiante con el legajo " << legajo << " no estaba inscripto." << std::endl;
+    for(int i = 0; i < lista_estudiantes.size(); i++){ 
+        if(lista_estudiantes[i] -> getLegajo() == legajo){ 
+            lista_estudiantes[i] -> eliminarCursoNota(nombre);
+            lista_estudiantes.erase(lista_estudiantes.begin() + i); 
+            return; 
+        } 
+    }
+
 }
 
+// Metodo para saber si un estudiante esta inscripto o no. -> Inciso b) -> ii)
+bool Curso::buscarEstudiante(int legajo){
+    for (int i; i < lista_estudiantes.size(); i++) if(lista_estudiantes[i] -> getLegajo() == legajo) return true; 
+    return false;
+}
+
+// Metodo para saber si el curso esta completo. -> Inceso b) -> iii)
+bool Curso::completo(){
+    if (lista_estudiantes.size() == capacidad_max){
+        std::cout << "El curso esta completo (hay 20 alumnos inscriptos)." << std::endl;
+        return true;
+    }
+    return false;
+}
